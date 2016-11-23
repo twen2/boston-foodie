99996b81610cfac9508e789162640b67f573c5ba
twen2 <twen2@wellesley.edu>
2016-11-24 03:14:39 +0800

    feature: general search & dish search; draft version

---
+++ search.cgi
@@ -0,0 +1,37 @@
+#!/usr/local/bin/python2.7
+
+import sys 
+import cgi
+import cgitb; cgitb.enable()
+import cgi_utils_sda
+import dbconn2
+import MySQLdb
+import search
+
+def main():
+    dsn = dbconn2.read_cnf(".my.cnf")
+    dsn['db'] = 'twen2_db'
+    dsn['host'] = 'localhost'
+    conn = dbconn2.connect(dsn)
+    conn.autocommit(True)
+    
+    form_data = cgi.FieldStorage()
+
+    display = ""
+    if ("generalS" in form_data):
+        display = search.generalS(conn, form_data)
+    elif ("dishS" in form_data):
+        if "dish" in form_data:
+            display = search.dishS(conn, form_data)
+        else:
+            display += "Please enter the dish name."
+    else:
+        display = ""
+
+    return display
+
+
+if __name__ == '__main__':
+    print 'Content-type: text/html\n'
+    # displays the webpage
+    print main()
---
+++ search.py
@@ -0,0 +1,86 @@
+#!/usr/local/bin/python2.7
+
+
+import sys 
+import cgi
+import cgitb; cgitb.enable()
+# import cgi_utils_sda
+import dbconn2
+import MySQLdb
+
+def generalS(conn, form_data):
+    curs = conn.cursor(MySQLdb.cursors.DictCursor)
+
+    loca = form_data.getfirst("location")
+    resType = form_data.getfirst("resType")
+    cuiType = form_data.getfirst("cuiType")
+
+    curs.execute('SELECT * FROM restaurants')
+    fullSet = curs.fetchall()
+    locaSet = fullSet
+    resSet = fullSet
+    cuiSet = fullSet
+
+    if loca != "Unspecified":
+        curs.execute('SELECT * FROM restaurants WHERE location = %s', (loca))
+        locaSet = curs.fetchall()
+
+    if resType != "Unspecified":
+        curs.execute('SELECT * FROM restaurants WHERE res_type = %s', (resType))
+        resSet = curs.fetchall()
+
+    if cuiType != "Unspecified":
+        curs.execute('SELECT * FROM restaurants WHERE cuisine_type = %s', (cuiType))
+        cuiSet = curs.fetchall()
+
+    resultSet = getResult(locaSet, resSet, cuiSet)
+    return resultSet
+
+
+def getResult(locaSet, resSet, cuiSet):
+    resultSet = []
+    for row in locaSet:
+        if (row in resSet) and (row in cuiSet):
+            resultSet.append(row["name"])
+
+    return resultSet
+
+def dishS(conn, form_data):
+    curs = conn.cursor(MySQLdb.cursors.DictCursor)
+    dish = form_data.getfirst("dish")
+    curs.execute('SELECT * FROM dishes WHERE name LIKE %s', ("%" + dish + "%"))
+    if curs.rowcount == 0:
+        return "Sorry, no dishes match your search."
+    else:
+        resultSet = curs.fetchall()
+        dishes = []
+        for result in resultSet:
+            dishes.append(result["name"])
+        return dishes
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
