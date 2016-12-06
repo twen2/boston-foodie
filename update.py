3d5935c467cd403a8d6f7f1d6a79210a38c0dac6
twen2 <twen2@wellesley.edu>
2016-12-06 17:01:33 -0500

    Update: duplicate restaurant and dish not allowed

--- update.py
+++ update.py
@@ -22,18 +22,22 @@ def insertRes(conn, form_data):
     curs = conn.cursor(MySQLdb.cursors.DictCursor)
 
     name = form_data.getfirst("resName")
-    loca = form_data.getfirst("loca")
-    cuisine = form_data.getfirst("cuisine")
-    resType = form_data.getfirst("resType")
 
-    # executes the given query that inserts the provided data into the table
-    curs.execute("""INSERT INTO restaurants(id, name, location, cuisine_type, res_type) VALUES (NULL, %s, %s, %s, %s)""",
-                (name, loca, cuisine, resType))
-    
-    return "The restaurant " + name + " is successfully inserted."
-    # executed the given query that returns the entry that matches the new insertion
-    # this query is used to check if the actor is inserted succesfully
-    # curs.execute('SELECT * FROM movie WHERE tt = %s', (tt))
+    if getRes(conn, name) == -1:
+        loca = form_data.getfirst("loca")
+        cuisine = form_data.getfirst("cuisine")
+        resType = form_data.getfirst("resType")
+
+        # executes the given query that inserts the provided data into the table
+        curs.execute("""INSERT INTO restaurants(id, name, location, cuisine_type, res_type) VALUES (NULL, %s, %s, %s, %s)""",
+                    (name, loca, cuisine, resType))
+        
+        return "The restaurant " + name + " is successfully inserted."
+        # executed the given query that returns the entry that matches the new insertion
+        # this query is used to check if the actor is inserted succesfully
+        # curs.execute('SELECT * FROM movie WHERE tt = %s', (tt))
+    else:
+        return "The restaurant " + name + " already exists."
 
 def insertDish(conn, form_data):
     curs = conn.cursor(MySQLdb.cursors.DictCursor)
@@ -44,7 +48,15 @@ def insertDish(conn, form_data):
     if resID == -1:
         return "Restaurant does not exist."
     else:
-        curs.execute("""INSERT INTO dishes(id, name, num_of_likes, res_id) VALUES (NULL, %s, %s, %s)""",
-                    (dish, 0, resID))
-        return "The dish " + dish + " is successfully inserted."
+        if not existDish(conn, resID, dish):
+            curs.execute("""INSERT INTO dishes(id, name, num_of_likes, res_id) VALUES (NULL, %s, %s, %s)""",
+                        (dish, 0, resID))
+            return "The dish " + dish + " is successfully inserted."
+        else:
+            return "The dish " + dish + " already exists for the restaurant " + resName + "."
 
+def existDish(conn, resID, dishName):
+    curs = conn.cursor(MySQLdb.cursors.DictCursor)
+
+    curs.execute("""SELECT * FROM dishes WHERE res_id = %s AND name = %s""", (resID, dishName))
+    return curs.rowcount != 0
